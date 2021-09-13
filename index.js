dda4789fbefd5a7819408df616ca4f9e6e6a241f
Arnonym <black38000@gmail.com>
2021-09-14 03:45:37 +0700

    Fix socket

--- index.js
+++ index.js
@@ -21,6 +21,26 @@ app.get('/todos', function (req, res) {
     })
 })
 
+app.get('/todos/max', function (req, res) {
+    let MongoClient = require('mongodb').MongoClient
+    MongoClient.connect(uri, function (err, db) {
+        if (err) throw err
+        let dbo = db.db('test')
+        dbo.collection('todos').find().sort({id: -1}).limit(1).toArray(function (err, result) {
+            if (err) throw err
+            if (result.length === 0)
+            {
+                res.send('[{ \"id\": 0 }]')
+            }
+            else
+            {
+                res.send(JSON.stringify(result))
+            }
+            db.close()
+        })
+    })
+})
+
 app.post('/todos/:id', function (req, res) {
     let MongoClient = require('mongodb').MongoClient
     MongoClient.connect(uri, function (err, db) {
@@ -37,6 +57,7 @@ app.post('/todos/:id', function (req, res) {
                 if (err) throw err
                 console.log("updated todo" + req.params.id)
                 res.send(JSON.stringify(result))
+                emit('u')
                 db.close()
             }
         )
@@ -54,6 +75,7 @@ app.delete('/todos/:id', function (req, res) {
                 if (err) throw err
                 console.log("deleted todo" + req.params.id)
                 res.send(JSON.stringify(result))
+                emit('u')
                 db.close()
             }
         )
@@ -75,6 +97,7 @@ app.put('/todos/:id', function (req, res) {
                 if (err) throw err
                 console.log("inserted todo" + req.params.id)
                 res.send(JSON.stringify(result))
+                emit(req.body.random.toString())
                 db.close()
             }
         )
@@ -83,7 +106,17 @@ app.put('/todos/:id', function (req, res) {
 
 app.use(express.static('./public'))
 
-function originIsAllowed(origin) {
+function emit(msg)
+{
+    console.log('broadcast to client from server')
+    for (const c of clients)
+    {
+        c.sendUTF(msg)
+    }
+}
+
+function originIsAllowed(origin)
+{
     return origin.indexOf('anakint.com') !== -1
 }
 
@@ -91,20 +124,15 @@ let clients = []
 
 ws.on('request', function (request) {
     console.log('new client connect to ws')
-    if (!originIsAllowed(request.origin))
-    {
-        console.log('Forbidden origin ' + request.origin + ' tried to connect to ws')
-        return
-    }
+    // if (!originIsAllowed(request.origin))
+    // {
+    //     console.log('Forbidden origin ' + request.origin + ' tried to connect to ws')
+    //     return
+    // }
     let conn = request.accept(null, request.origin)
     clients.push(conn)
-    
-    conn.on('message', (msg) => {
-        console.log('broadcasting update to clients')
-        for (const c of clients)
-        {
-            c.sendUTF('u')
-        }
+    conn.on('close', () => {
+        clients.splice(clients.indexOf(conn), 1)
     })
 })
 
